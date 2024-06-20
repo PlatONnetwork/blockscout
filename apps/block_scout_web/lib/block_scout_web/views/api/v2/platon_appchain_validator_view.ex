328506c0583f269fa6f314dd6c44e953755eb700
hudenian <hudenian@163.com>
2024-06-20 12:06:23 +0800

    历史验证人列表去掉多余的括号

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/platon_appchain_validator_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/platon_appchain_validator_view.ex
@@ -50,7 +50,11 @@ defmodule BlockScoutWeb.API.V2.PlatonAppchainValidatorView do
 
   @spec render(String.t(), map()) :: map()
   def render("platon_appchain_history_validators.json", %{validators: validators, next_page_params: next_page_params}) do
-    items = Enum.with_index(validators) |> Enum.map(fn {validator, index} -> {convert_l2_history_validator(validator) |> Map.put("index", index + 1)} end)
+    items = Enum.with_index(validators)
+            |> Enum.map(fn {validator, index} ->
+      convert_l2_history_validator(validator)
+      |> Map.put("index", index + 1)
+    end)
     %{items: items, next_page_params: next_page_params}
   end
 
