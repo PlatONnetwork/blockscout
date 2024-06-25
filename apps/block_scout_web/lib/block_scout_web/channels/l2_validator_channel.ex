141f6dbf000244027adca13fb48c574aed82bea0
hudenian <hudenian@163.com>
2024-06-25 11:34:36 +0800

    给前端推送验证总数信息

--- apps/block_scout_web/lib/block_scout_web/channels/l2_validator_channel.ex
+++ apps/block_scout_web/lib/block_scout_web/channels/l2_validator_channel.ex
@@ -10,11 +10,10 @@ defmodule BlockScoutWeb.L2ValidatorChannel do
     "active_validator",
     "candidate_validator",
     "history_validator",
-    "allvalidator",
   ])
 
   def join("platon_appchain_l2_validator:all_validator", _params, socket) do
-    {:ok, %{"test"=>"test"}, socket}
+    {:ok, %{}, socket}
   end
 
   def join("platon_appchain_l2_validator:active_validator", _params, socket) do
@@ -36,16 +35,12 @@ defmodule BlockScoutWeb.L2ValidatorChannel do
 #  })
   #    test end
   def handle_out(
-        "allvalidator",
+        "all_validator",
         validatorsCount,
         %Phoenix.Socket{handler: BlockScoutWeb.UserSocketV2} = socket
       ) do
 
-    Logger.info(fn -> "hander out all_validator>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" end ,
-      logger: :platon_appchain
-    )
-#    result_validator = Enum.map(validators, fn validator -> convert_l2_validator(validator) end)
-    push(socket, "allvalidator", validatorsCount)
+    push(socket, "all_validator", %{count: validatorsCount})
     {:noreply, socket}
   end
 
@@ -54,8 +49,7 @@ defmodule BlockScoutWeb.L2ValidatorChannel do
         activeValidatorsCount,
         %Phoenix.Socket{handler: BlockScoutWeb.UserSocketV2} = socket
       ) do
-#    result_validator = Enum.map(validators, fn validator -> convert_l2_validator(validator) end)
-    push(socket, "active_validator", activeValidatorsCount)
+    push(socket, "active_validator", %{count: activeValidatorsCount})
     {:noreply, socket}
   end
 
@@ -65,7 +59,7 @@ defmodule BlockScoutWeb.L2ValidatorChannel do
         %Phoenix.Socket{handler: BlockScoutWeb.UserSocketV2} = socket
       ) do
 #    result_validator = Enum.map(validators, fn validator -> convert_l2_validator(validator) end)
-    push(socket, "candidate_validator", validatorsCount)
+    push(socket, "candidate_validator", %{count: validatorsCount})
     {:noreply, socket}
   end
 
@@ -74,50 +68,7 @@ defmodule BlockScoutWeb.L2ValidatorChannel do
         historyValidatorsCount,
         %Phoenix.Socket{handler: BlockScoutWeb.UserSocketV2} = socket
       ) do
-#    result_history_validator = Enum.with_index(historyValidators) |> Enum.map(fn {validator, index} -> {convert_l2_history_validator(validator) |> Map.put("index", index + 1)} end)
-#    push(socket, "history_validator", result_history_validator)
-    push(socket, "history_validator", historyValidatorsCount)
+    push(socket, "history_validator", %{count: historyValidatorsCount})
     {:noreply, socket}
   end
-
-# 不在这里定义，参考：apps/block_scout_web/lib/block_scout_web/views/api/v2/platon_appchain_validator_view.ex中的定义
-#  defp convert_l2_validator(validator) do
-#    %{
-#      "rank" => validator.rank,
-#      "validators" => validator.validator_hash,
-#      "status" => validator.status, # 0: 正常 1：无效 2：低出块 4: 低阈值 8: 双签 32：解质押 64:惩罚
-#      "stake_epoch" => validator.stake_epoch,
-#      "owner_hash" => validator.owner_hash,
-#      "commission" => validator.commission_rate,
-#      "stake_amount" => validator.stake_amount, # 有效质押金额
-#      "locking_stake_amount" => validator.locking_stake_amount, # 锁定的质押金额（解除的部分质押，需要锁定一段时间）
-#      "withdrawal_stake_amount" => validator.withdrawal_stake_amount, # 可提取的质押金额
-#      "delegate_amount" => validator.delegate_amount, # 有效委托金额
-#      "stake_reward" => validator.stake_reward,
-#      "delegate_reward" => validator.delegate_reward,
-#      "name" => validator.name,
-#      "detail" => validator.detail,
-#      "logo" => validator.logo,
-#      "website" => validator.website,
-#      "expect_apr" => validator.expect_apr,
-#      "block_rate" => validator.block_rate,
-#      "auth_status" => validator.auth_status, #  是否验证 0-未验证，1-已验证
-#      "role" => validator.role, # 0-candidate(质押节点) 1-active(共识节点候选人) 2-verifying(共识节点)
-#      "block_rate" => validator.block_rate,
-#      "total_bonded_amount" => "总质押金额",
-#      "total_bonded_percent" => "总质押占所有质押比",
-#    }
-#  end
-#
-#  defp convert_l2_history_validator(validator) do
-#    %{
-#      "no" => validator.stake_epoch,
-#      "validators" => validator.validator_hash,
-#      "status" => validator.status, # 0: 正常 1：无效 2：低出块 4: 低阈值 8: 双签 32：解质押 64:惩罚
-#      "exit_block" => validator.exit_block,
-#      "exit_timestamp" => validator.timestamp,
-#      "event" => validator.exit_desc,
-#    }
-#  end
-
 end
