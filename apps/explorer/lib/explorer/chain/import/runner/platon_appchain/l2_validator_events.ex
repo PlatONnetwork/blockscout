c42b947f5983ef832c9d7e280891d991a4486fd7
formingform <formingform.lyu@gmail.com>
2025-09-03 16:56:57 +0800

    add debug

--- apps/explorer/lib/explorer/chain/import/runner/platon_appchain/l2_validator_events.ex
+++ apps/explorer/lib/explorer/chain/import/runner/platon_appchain/l2_validator_events.ex
@@ -79,8 +79,6 @@ defmodule Explorer.Chain.Import.Runner.PlatonAppchain.L2ValidatorEvents do
     %{ true => registered_events } = registered_events_and_others
     Logger.debug("to import L2ValidatorEvents (registered_events):  #{inspect(registered_events)}")
 
-    when is_list(registered_events)
-
     import_result =
     multi
     |> Multi.run(:insert_l2_validator_events, fn repo, _ ->
