9b1d0b3c992a755ead4d3d0e5f38d86c3bd2b6df
formingform <formingform.lyu@gmail.com>
2025-09-17 18:13:27 +0800

    from/to is optional

--- apps/explorer/lib/explorer/chain/platon_appchain/l2_event.ex
+++ apps/explorer/lib/explorer/chain/platon_appchain/l2_event.ex
@@ -3,7 +3,7 @@ defmodule Explorer.Chain.PlatonAppchain.L2Event do
 
   alias Explorer.Chain.{Address, Block, Hash, Wei}
 
-  @optional_attrs ~w(amount block_timestamp, from, to)a
+  @optional_attrs ~w(amount block_timestamp from to)a
 
   @required_attrs ~w(event_id tx_type hash block_number)a
 
