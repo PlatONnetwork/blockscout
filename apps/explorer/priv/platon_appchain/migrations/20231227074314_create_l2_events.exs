5c6a76c5dfa4a1d0fa21be8c8d9eca225b6056c3
formingform <formingform.lyu@gmail.com>
2025-09-17 18:20:16 +0800

    from/to is optional

--- apps/explorer/priv/platon_appchain/migrations/20231227074314_create_l2_events.exs
+++ apps/explorer/priv/platon_appchain/migrations/20231227074314_create_l2_events.exs
@@ -13,9 +13,9 @@ defmodule Explorer.Repo.PlatonAppchain.Migrations.CreateL2Txs do
       # l2上交易hash
       add(:hash, :bytea, null: false)
       # 交易发起者
-      add(:from, :bytea, null: false)
+      add(:from, :bytea, null: true)
       # 交易接收者(l1上面接收合约)
-      add(:to, :bytea, null: false)
+      add(:to, :bytea, null: true)
       # 交易所在区块
       add(:block_number, :bigint, null: false)
       # 交易时间 客户端算根据此值计算（age）
